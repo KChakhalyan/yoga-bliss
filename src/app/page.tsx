003dfd013005b93728e4ef39d4918ab22c8e9283
KChakhalyan <karenchakhalyan23@gmail.com>
2025-05-02 14:29:03 +0400

    Add Gallery section and update Schedule table styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Schedule from "./sections/Schedule";
+import Gallery from "./sections/Gallery";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <About />
       <Schedule />
+      <Gallery />
     </>
   );
 }
--- src/app/sections/Gallery.tsx
+++ src/app/sections/Gallery.tsx
@@ -1,6 +1,54 @@
+import Image from "next/image"
+
 const Gallery = () => {
     return (
-        <div>Gallery</div>
+        <section className="py-20 flex flex-col item-center justyfy-center">
+            <h1 className="text-4xl text-center font-serif mb-10">Our Studio</h1>
+            <div className="grid sm:grid-cols-3 gap-6 grid-cols-2 justify-items-center">
+                <Image
+                    src={"/hero.png"}
+                    alt="Yoga Bliss"
+                    width={300}
+                    height={200}
+                    className="rounded-lg shadow-md object-cover transition-transform duration-300 transform hover:scale-105"
+                />
+                <Image
+                    src={"/hero.png"}
+                    alt="Yoga Bliss"
+                    width={300}
+                    height={200}
+                    className="rounded-lg shadow-md object-cover transition-transform duration-300 transform hover:scale-105"
+                />
+                <Image
+                    src={"/hero.png"}
+                    alt="Yoga Bliss"
+                    width={300}
+                    height={200}
+                    className="rounded-lg shadow-md object-cover transition-transform duration-300 transform hover:scale-105"
+                />
+                <Image
+                    src={"/hero.png"}
+                    alt="Yoga Bliss"
+                    width={300}
+                    height={200}
+                    className="rounded-lg shadow-md object-cover transition-transform duration-300 transform hover:scale-105"
+                />
+                <Image
+                    src={"/hero.png"}
+                    alt="Yoga Bliss"
+                    width={300}
+                    height={200}
+                    className="rounded-lg shadow-md object-cover transition-transform duration-300 transform hover:scale-105"
+                />
+                <Image
+                    src={"/hero.png"}
+                    alt="Yoga Bliss"
+                    width={300}
+                    height={200}
+                    className="rounded-lg shadow-md object-cover transition-transform duration-300 transform hover:scale-105"
+                />
+            </div>
+        </section>
     )
 }
 export default Gallery
